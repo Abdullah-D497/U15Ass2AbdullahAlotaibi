fc04d800eb90f22dfdba3ea2675ea4b8ed999eb1
Abdullah-D497 <Abdullahsaud0743@gmail.com>
2026-06-26 11:25:02 +0100

    Date 26/06

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,3 +1,16 @@
 function sayWelcome() {
     alert("Welcome")
 }
+
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+
+  if (name === "" || email === "") {
+    document.getElementById("message").innerHTML = "Please fill all fields!";
+    return false;
+  } else {
+    document.getElementById("message").innerHTML = "Form submitted successfully!";
+    return true;
+  }
+}
